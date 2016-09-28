5a1e220677250c5b453ab6c0b2a94680a245ca36
Jeff Morse <edward.tohr@gmail.com>
2016-09-27 21:25:18 -0400

    Add a few more placeholders to main

--- main.java
+++ main.java
@@ -4,7 +4,7 @@
  }
 
  public enum Ai {
- AI_DEFAULT, AI_RECKLESS, AI_CAUTIOUS
+ AI_DEFAULT, AI_RECKLESS, AI_CAUTIOUS, AI_CASTER_FIRE, AI_CASTER_ICE, AI_CASTER_LIGHTNING, AI_CASTER_HEAL
  }
 
  public enum Role {
@@ -12,7 +12,7 @@
  }
 
  public enum EquipSlot {
- SLOT_NONE, SLOT_HEAD, SLOT_BODY, SLOT_HANDS, SLOT_LEGS, SLOT_FEET, SLOT_WEAPON, SLOT_OTHER
+ SLOT_NONE, SLOT_HEAD, SLOT_BODY, SLOT_HANDS, SLOT_LEGS, SLOT_FEET, SLOT_WEAPON, SLOT_OTHER, SLOT_HAT
  }
 
  public enum ObjectType {
@@ -25,4 +25,8 @@
 
  int randInt(int low, int high){
  // generate a random integer between low and high
+ }
+
+ String getInput(){
+ // allow typing into text entry box, when enter is hit, return text in box.
  }
